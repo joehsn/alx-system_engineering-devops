09629a98e37222d34bf76c8a22519f8ab751167e
Youssef Hassan <youssef.hassan.mahmoud@gmail.com>
2024-03-22 06:22:13 -0700

    0. Create a file: requirment failed

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,10 @@
+# This Puppet manifest creates a file named /tmp/school with specific permissions and content
+
 file { '/tmp/school':
   ensure  => present,
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet',
+  content => 'I love Puppet\n',  # Add a newline character at the end
 }
+
