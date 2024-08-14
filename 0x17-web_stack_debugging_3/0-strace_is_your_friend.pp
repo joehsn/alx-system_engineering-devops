90d88b15ede131885a9e937cfbaf19ca564a7e26
joehsn <joehsn@outlook.com>
2024-08-14 20:46:29 +0000

    0x17. Web stack debugging #3 task is done

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Debugging an Apache server returning a 500 error
+
+exec { 'Debugging an apache server':
+    command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+    path    => '/var/www/html/'
+}
